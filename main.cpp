79281ad4e156ef6f9890d3d1624c6b58ba7fbbc3
Chris Gregory <czipperz@gmail.com>
2021-10-20 20:01:08 -0700

    Use WinMain to avoid creating a console on windows

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 int actual_main(int argc, char** argv);
 
+#if defined(_WIN32) && !defined(CONSOLE_MAIN)
+#include <windows.h>
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+                   _In_opt_ HINSTANCE hPrevInstance,
+                   _In_ LPSTR lpCmdLine,
+                   _In_ int nShowCmd) {
+    return actual_main(__argc, __argv);
+}
+#else
 int main(int argc, char** argv) {
     return actual_main(argc, argv);
 }
+#endif
