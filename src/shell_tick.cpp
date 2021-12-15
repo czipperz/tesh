74e7846318b4b6947f9c214baae5a0933e7ca1d3
Chris Gregory <czipperz@gmail.com>
2021-12-14 23:15:02 -0800

    Refactor

--- src/shell_tick.cpp
+++ src/shell_tick.cpp
@@ -188,9 +188,9 @@ static bool tick_program(Shell_State* shell,
 
         // TODO merge bg jobs up???
         if (node->fg_finished && node->bg.len == 0) {
-            *exit_code = program->v.sub.fg.last_exit_code;
-            cleanup_local(program->v.sub.local);
-            cleanup_stdio(&program->v.sub.stdio);
+            *exit_code = node->fg.last_exit_code;
+            cleanup_local(node->local);
+            cleanup_stdio(&node->stdio);
             return true;
         }
     } break;
