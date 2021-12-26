7b2d77b62633a41a1f6a5ad3c415754b6df9d823
Chris Gregory <czipperz@gmail.com>
2021-12-25 22:27:59 -0800

    mycat: Allow specifying a file path

--- tests/mycat.c
+++ tests/mycat.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char** argv) {
+    FILE* file = stdin;
+    if (argc >= 2) {
+        file = fopen(argv[1], "r");
+        if (!file) {
+            fprintf(stderr, "Error: couldn't open %s\n", argv[1]);
+            return 1;
+        }
+    }
     printf("mycat: ");
     while (1) {
-        int ch = getchar();
+        int ch = fgetc(file);
         if (ch == EOF) {
             break;
         }
