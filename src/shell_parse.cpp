86f5a4fecc4c9a377aef83a895ba3520fa081812
Chris Gregory <czipperz@gmail.com>
2022-03-12 18:13:25 -0800

    Handle unquoted '\*' -> '*'

--- src/shell_parse.cpp
+++ src/shell_parse.cpp
@@ -1053,7 +1053,7 @@ static void expand_arg(const Shell_Local* local,
             if (index < text.len) {
                 char c2 = text[index];
                 if (c2 == '"' || c2 == '\\' || c2 == '`' || c2 == '$' || c2 == ' ' || c2 == '~' ||
-                    c2 == '&') {
+                    c2 == '&' || c2 == '*') {
                     word->reserve(allocator, 1);
                     word->push(c2);
                     ++index;
