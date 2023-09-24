a1f97da8f5dd8e6aeed4794894bbd1a2a13d6237
Chris Gregory <czipperz@gmail.com>
2023-09-24 03:57:13 -0400

    Refactor execution test

--- tests/test_shell_execute.cpp
+++ tests/test_shell_execute.cpp
@@ -11,6 +11,13 @@ static Render_State rend = {};
 static Prompt_State command_prompt = {};
 static cz::Vector<Backlog_State*> backlogs = {};
 
+struct Test_Info {
+    cz::Str command;
+    int expected_exit_code;
+    cz::Str expected_output;
+};
+static cz::Vector<Test_Info> tests = {};
+
 static void setup_environment() {
     temp_arena.init();
     temp_allocator = temp_arena.allocator();
@@ -24,7 +31,7 @@ static void setup_environment() {
     rend.selected_outer = -1;
 }
 
-static void execute(cz::Str command) {
+static void define_test(cz::Str command, int expected_exit_code, cz::Str expected_output) {
     Backlog_State* backlog = cz::heap_allocator().alloc<Backlog_State>();
     *backlog = {};
     init_backlog(backlog, backlogs.len, /*max_length=*/1ull << 30 /*1GB*/);
@@ -32,6 +39,9 @@ static void execute(cz::Str command) {
 
     backlogs.reserve(cz::heap_allocator(), 1);
     backlogs.push(backlog);
+
+    tests.reserve(cz::heap_allocator(), 1);
+    tests.push({command, expected_exit_code, expected_output});
 
     bool result = run_script(&shell, backlog, command);
     if (!result) {
@@ -50,48 +60,24 @@ static void wait_for_scripts_to_finish() {
 TEST_CASE("execution tests") {
     setup_environment();
 
-    bool first = true;
+    define_test("file=$(mktemp) && echo hi | cat > $file && cat $file",  //
+                /*exit_code=*/0, /*output=*/"hi\n");
 
-top:
-    size_t i = 0;
+    define_test("echo $(echo one | cat); echo $(echo two | cat) | cat",
+                /*exit_code=*/0, /*output=*/"one\ntwo\n");
 
-    if (first) {
-        execute("file=$(mktemp) && echo hi | cat > $file && cat $file");
-    } else {
-        CHECK(backlogs[i]->exit_code == 0);
-        CHECK(dbg_stringify_backlog(backlogs[i]) == "hi\n");
-        ++i;
-    }
+    define_test("a=1; c=1; (a=2; b=2; echo \"$a $b $c\"); echo \"$a $b $c\"\n",
+                /*exit_code=*/0, /*output=*/"2 2 1\n1  1\n");
 
-    if (first) {
-        execute("echo $(echo one | cat); echo $(echo two | cat) | cat");
-    } else {
-        CHECK(backlogs[i]->exit_code == 0);
-        CHECK(dbg_stringify_backlog(backlogs[i]) == "one\ntwo\n");
-        ++i;
-    }
-
-    if (first) {
-        execute("a=1; c=1; (a=2; b=2; echo \"$a $b $c\"); echo \"$a $b $c\"\n");
-    } else {
-        CHECK(backlogs[i]->exit_code == 0);
-        CHECK(dbg_stringify_backlog(backlogs[i]) == "2 2 1\n1  1\n");
-        ++i;
-    }
-
-    if (first) {
-        execute("a=1; c=1; (a=2; b=2; unset c; echo \"$a $b $c\"); echo \"$a $b $c\"\n");
-    } else {
-        CHECK(backlogs[i]->exit_code == 0);
-        CHECK(dbg_stringify_backlog(backlogs[i]) == "2 2 \n1  1\n");
-        ++i;
-    }
-
-    if (!first)
-        return;
+    define_test("a=1; c=1; (a=2; b=2; unset c; echo \"$a $b $c\"); echo \"$a $b $c\"\n",
+                /*exit_code=*/0, /*output=*/"2 2 \n1  1\n");
 
     wait_for_scripts_to_finish();
 
-    first = false;
-    goto top;
+    for (size_t i = 0; i < tests.len; ++i) {
+        INFO("test index: " << i);
+        INFO("command: " << tests[i].command.buffer);
+        CHECK(backlogs[i]->exit_code == tests[i].expected_exit_code);
+        CHECK(dbg_stringify_backlog(backlogs[i]) == tests[i].expected_output);
+    }
 }
